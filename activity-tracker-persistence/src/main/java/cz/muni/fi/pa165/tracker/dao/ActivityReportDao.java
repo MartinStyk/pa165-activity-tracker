d7bdf6e1c5409ae05459f9308d2f220a25a3b5af
Ondrejkova <408256@mail.muni.cz>
2016-10-19 15:46:20 +0200

    [ADD] create class ActivityReport with most parameters but not all [ADD] create interface ActivityReportDao with describtion all function [ADD] create class ActivityReportDaoImpl, function are not implemented yet

---
+++ activity-tracker-persistence/src/main/java/cz/muni/fi/pa165/tracker/dao/ActivityReportDao.java
@@ -0,0 +1,81 @@
+package cz.muni.fi.pa165.tracker.dao;
+
+import cz.muni.fi.pa165.tracker.entity.ActivityReport;
+import cz.muni.fi.pa165.tracker.entity.User;
+import java.util.List;
+
+/**
+ * Interface represents data acces object of activity report
+ * TODO findReportBySport and deleteRepotsBySport
+ * 
+ * @author Petra Ondřejková
+ * @version 18.10.2016
+ */
+public interface ActivityReportDao {
+    
+    /**
+     * Method that create activity report in DB
+     *
+     * @param activityReport report to be create
+     */
+    void create(ActivityReport activityReport);
+
+    /**
+     * Method that find activity record by id
+     *
+     * @param id of the report
+     * @return activity report with given id
+     */
+    ActivityReport findActivityReportByID(Long id);
+
+    /**
+     * Method that finds all reports of an user.
+     *
+     * @param user whose record will be returned
+     * @return collection of user's reports
+     */
+    List<ActivityReport> findRecordsByUser(User user);
+
+    /**
+     * Method that finds records by Sport Activity
+     *
+     * @param activity which reports will be returned
+     * @return collection of reports of activity
+     */
+    //List<ActivityReport> findRecordsBySportActivity(SportActivity activity);
+
+    /**
+     * Method that deletes activity report form DB
+     *
+     * @param actvityReport report to be deleted
+     */
+    void delete(ActivityReport actvityReport);
+
+    /**
+     * Method that deletes all user's activities reports
+     *
+     * @param user whose reports will be deleted
+     */
+    void deleteUserReports(User user);
+
+    /**
+     * Method that deletes reports by sport activity.
+     *
+     * @param activity by which the reports will be deleted
+     */
+    // void deleteReportsBySportActivity(SportActivity activity);
+
+    /**
+     * Method that updates activity report in DB
+     *
+     * @param activityReport report to be updated
+     */
+    void update(ActivityReport activityReport);
+
+    /**
+     * Method that finds all reports in DB
+     *
+     * @return list of all activity reports 
+     */
+    List<ActivityReport> findAll();
+}
---
+++ activity-tracker-persistence/src/main/java/cz/muni/fi/pa165/tracker/dao/ActivityReportDaoImpl.java
@@ -0,0 +1,57 @@
+package cz.muni.fi.pa165.tracker.dao;
+
+import cz.muni.fi.pa165.tracker.entity.ActivityReport;
+import cz.muni.fi.pa165.tracker.entity.User;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Class represents implementation of dao activity report in application
+ * TODO implements all method
+ * 
+ * @author Petra Ondřejková
+ * @version 18.10.2016
+ */
+public class ActivityReportDaoImpl implements ActivityReportDao {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    
+    @Override
+    public void create(ActivityReport activityReport) {
+        em.persist(activityReport);
+    }
+
+    @Override
+    public ActivityReport findActivityReportByID(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<ActivityReport> findRecordsByUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(ActivityReport actvityReport) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteUserReports(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(ActivityReport activityReport) {
+        em.merge(activityReport);
+    }
+
+    @Override
+    public List<ActivityReport> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
