3fa70edbc5801d771d7cb9ed6a5ed59cb3b1d9a7
Martin Styk <martin.styk@gmail.com>
2016-11-11 15:13:29 +0100

    Configure Dozer to work with Java8 time API

--- activity-tracker-service/src/main/java/cz/muni/fi/pa165/tracker/ServiceConfiguration.java
+++ activity-tracker-service/src/main/java/cz/muni/fi/pa165/tracker/ServiceConfiguration.java
@@ -1,13 +1,23 @@
 package cz.muni.fi.pa165.tracker;
 
 
+import cz.muni.fi.pa165.tracker.dto.UserCreateDTO;
+import cz.muni.fi.pa165.tracker.dto.UserDTO;
+import cz.muni.fi.pa165.tracker.entity.User;
 import cz.muni.fi.pa165.tracker.exception.DataAccessExceptionTranslateAspect;
 import cz.muni.fi.pa165.tracker.facade.ActivityReportFacadeImpl;
+import cz.muni.fi.pa165.tracker.mapping.BeanMappingService;
+import cz.muni.fi.pa165.tracker.mapping.BeanMappingServiceImpl;
 import cz.muni.fi.pa165.tracker.service.ActivityReportServiceImpl;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.dozer.loader.api.FieldDefinition;
+import org.dozer.loader.api.TypeMappingOptions;
+import org.springframework.context.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Martin Styk
@@ -17,6 +27,17 @@ import org.springframework.context.annotation.Import;
 @EnableAspectJAutoProxy
 @Import(PersistenceApplicationContext.class)
 @ComponentScan(basePackageClasses = {ActivityReportServiceImpl.class, ActivityReportFacadeImpl.class,
-        DataAccessExceptionTranslateAspect.class})
+        DataAccessExceptionTranslateAspect.class, BeanMappingServiceImpl.class})
 public class ServiceConfiguration {
+
+    @Bean
+    public Mapper dozer() {
+        // this is needed to support Java 8 time api with Dozer
+        List<String> mappingFiles = new ArrayList();
+        mappingFiles.add("dozerJdk8Converters.xml");
+
+        DozerBeanMapper dozer = new DozerBeanMapper();
+        dozer.setMappingFiles(mappingFiles);
+        return dozer;
+    }
 }
